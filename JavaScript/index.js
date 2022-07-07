25bbe568043cc10e6cb444b883102b42ba74751a
Jatoth vinod <90764824+munnanayak@users.noreply.github.com>
2022-07-07 20:42:31 +0530

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,73 @@
+// JavaScript console.API(application progamming interface)
+//console.log("Hello World",5+1,"Another log")
+//console.warn("this is warning")
+//console.error("this is an error")
+
+//1.JavaScript Variables
+//what are vareables-container to store data values
+
+/* var number1 = 30;
+var number2 = 2;
+console.log("number1 + number2")
+// String
+var str1 = "this is me";
+var str2 = "this is nayak";
+
+// objects
+var marks {
+    nayak : 88,
+    vinod : 77,
+    munna : 55,
+}
+console.log(marks); 
+
+var a = 50;
+var b = 60;
+console.log("the value of a and b", a+b);
+console.log("the value of a and b", a-b);
+console.log("the value of a and b", a*b);
+console.log("the value of a and b", a/b); 
+// Functions
+
+function avg(a,b){
+       return (a+b)/2;
+}
+  c1 = avg(4,5);
+  c2 = avg(15,6);
+  console.log(c1,c2);*/
+
+  //Arrays in JavaScript
+     
+  //var arr = [1,2,3,4,5,6,7,8,9];
+  //console.log(arr);
+  //for(var i = 0;i<arr.length;i++){
+   //   console.log(arr[i]);
+  //}
+   //arr.forEach(function(element){
+  //    console.log(element);
+   //})
+  // let j=0;
+   //while(j<arr.length){
+    //   console.log(arr[j]);
+    //   j++;
+  // }
+  //let j=2;
+  //do{
+  //   console.log(arr[j]);
+   //   j++;
+ // }
+  //while(j<arr.length);
+  //let myArr=["Nayak","anil",33,null,true];
+  //myArr Methodes
+  //console.log(myArr.length);
+  // myArr.pop(); 
+  //myArr.push("Vinod")
+  //myArr.shift();
+  //const newLen = myArr.unshift("vinod")
+  //console.log(newLen);
+
+  //console.log(myArr);
+
+ // String Methodes in JavaScript
+ let myString = "Nayak is a good boy";
+ console.log(myString.lenght);
